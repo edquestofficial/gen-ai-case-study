aa7cac0cf809dd6587189cd58b128a2342b346c8
Mohit <127677249+mohitkumarr60@users.noreply.github.com>
2024-09-03 12:09:30 +0530

    Update app.py

--- case_study/8_financial_document_analysis/app.py
+++ case_study/8_financial_document_analysis/app.py
@@ -1,11 +1,12 @@
-from llama_index.core import SimpleDirectoryReader, VectorStoreIndex  # Importing the necessary classes for reading documents and creating a vector index
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding  # Importing the class for generating embeddings using a Hugging Face model
-from llama_index.core import Settings, StorageContext  # Importing settings and storage context for configuring the embedding model and vector store
-from llama_index.llms.gemini import Gemini  # Importing the Gemini LLM for generating responses
-from llama_index.vector_stores.redis import RedisVectorStore  # Importing the Redis vector store for storing embeddings
-from dotenv import load_dotenv  # Importing to load environment variables from a .env file
-import os  # Importing the os module to access environment variables
-import redis  # Importing the Redis library for connecting to the Redis server
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.core import Settings, StorageContext
+from llama_index.llms.gemini import Gemini
+from llama_index.vector_stores.redis import RedisVectorStore
+from dotenv import load_dotenv
+import os
+import redis
+from redisvl.schema import IndexSchema
 
 def main():
     load_dotenv()  # Load environment variables from the .env file
@@ -22,6 +23,32 @@ def main():
     # Load documents from the specified directory with parallel processing
     documents = SimpleDirectoryReader(input_dir='./data').load_data(num_workers=4)
     
+    custom_schema = IndexSchema.from_dict(
+      {
+        'index':{
+            'name':'financial_data',
+            'prefix': 'data',
+            'key_separator': ':'
+        },
+        'fields':[
+          {"type": "tag", "name": "id"},
+          {"type": "tag", "name": "doc_id"},
+          {"type": "text", "name": "text"},
+          {"type": "numeric", "name": "updated_at"},
+          {"type": "tag", "name": "file_name"}, 
+          {
+            'type':'vector',
+            'name':'vector',
+            'attrs':{
+                'dims': 384,
+                'algorithm':'hnsw',
+                'distance_metric': 'cosine',
+            },
+          }
+        ]
+      }
+    )
+
     # Print the number of loaded documents
     length = len(documents)
     print(f"Loaded {length} documents")
@@ -31,11 +58,16 @@ def main():
     Settings.llm = Gemini(api_key=os.getenv("GEMINI_API_KEY"), model="models/gemini-pro")
 
     # Initialize the Redis vector store and storage context
-    vector_store = RedisVectorStore(redis_client=redis_client, overwrite=True)
+    vector_store = RedisVectorStore(schema=custom_schema,redis_client=redis_client, overwrite=True)
     storage_context = StorageContext.from_defaults(vector_store=vector_store)
 
     # Create the index from the document embeddings with progress display
     index = VectorStoreIndex.from_documents(documents, storage_context=storage_context, show_progress=True)
+
+    #query engine
+    query_engine = index.as_query_engine()
+    response = query_engine.query("What did the author learn?")
+    print(response)
 
 if __name__ == "__main__":
     main()  # Run the main function
