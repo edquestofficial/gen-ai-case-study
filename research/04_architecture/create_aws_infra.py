572e724ec037539784c690289134870eed3b7219
Raman <rmnchopra91@gmail.com>
2024-12-20 21:40:33 +0530

    cicd

--- research/04_architecture/create_aws_infra.py
+++ research/04_architecture/create_aws_infra.py
@@ -5,9 +5,10 @@ from aws_lambda import  create_update_lambda
 if __name__ == "__main__":
     # Step 1: Create IAM role
     # create_bucket_new1.create_s3_bucket("gen-ai-poc-73033558046723", "us-east-1")
-    bucket_name = "my-edquest-bucket-name-11"  
+    bucket_name = "my-edquest-bucket-name-111"
+    region = "us-east-1"
     print(f"bucket name -- ", bucket_name)
-    create_bucket.create_s3_bucket(bucket_name)
+    create_bucket.create_s3_bucket(bucket_name, region)
     role_arn = create_update_role.create_iam_role()
 
     # Step 2: Deploy Lambda function
