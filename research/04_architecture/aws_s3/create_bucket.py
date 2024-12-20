6cca15301300c3283bae0010fa26407a1ed53146
Raman <rmnchopra91@gmail.com>
2024-12-20 21:27:37 +0530

    cicd

--- research/04_architecture/aws_s3/create_bucket.py
+++ research/04_architecture/aws_s3/create_bucket.py
@@ -1,18 +1,17 @@
 import boto3
 from botocore.exceptions import ClientError
 
-def create_s3_bucket(bucket_name, region=None):
-    """
-    Check if an S3 bucket exists by listing buckets and create it if not.
-    
-    Args:
-    - bucket_name: The name of the bucket to create.
-    - region: The AWS region for the bucket (e.g., 'us-east-1').
-    
-    Returns:
-    - str: Message indicating the result.
-    """
-    print(f"bucket_name : {bucket_name}")
-    print(f"region : {region}")
-    s3_client = boto3.client('s3', region_name=region)
-    s3_client.create_bucket(Bucket=bucket_name)
+# bucket_name = "my-edquest-bucket-name-1"  # Replace with a unique bucket name
+# region = "${{ secrets.AWS_REGION }}"  # Replace with your region or use GitHub Secrets
+
+def create_s3_bucket(bucket_name, region='us-east-1'):
+    print(f"bucket_name --- ", bucket_name)
+    print(f"region --- ", region)
+    try:
+        s3_client = boto3.client("s3", region_name=region)
+        s3_client.create_bucket(Bucket=bucket_name)
+    except ClientError as e:
+        print(f"Failed to create bucket: {e}")
+        raise
+
+create_s3_bucket(bucket_name)
--- research/04_architecture/create_aws_infra.py
+++ research/04_architecture/create_aws_infra.py
@@ -1,12 +1,16 @@
-from aws_s3 import create_bucket_new1
+from aws_s3.create_bucket import create_s3_bucket
 from aws_roles import create_update_role
 from aws_lambda import create_update_lambda
 
+print("Hello world")
+bucket_name = "my-edquest-bucket-name-1" 
+print(f"bucket_name --- ", bucket_name)
+create_s3_bucket(bucket_name)
 
-if __name__ == "__main__":
-    # Step 1: Create IAM role
-    create_bucket_new1.create_s3_bucket("gen-ai-poc-73033558046723", "us-east-1")
-    role_arn = create_update_role.create_iam_role()
+# if __name__ == "__main__":
+#     # Step 1: Create IAM role
+#     create_bucket_new1.create_s3_bucket("gen-ai-poc-73033558046723", "us-east-1")
+#     role_arn = create_update_role.create_iam_role()
 
-    # Step 2: Deploy Lambda function
-    create_update_lambda.create_or_update_lambda_function(role_arn)
+#     # Step 2: Deploy Lambda function
+#     create_update_lambda.create_or_update_lambda_function(role_arn)
