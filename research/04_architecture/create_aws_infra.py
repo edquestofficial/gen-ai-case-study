6f9919c7246b48882664682f7971fc81f7e7e901
Raman <rmnchopra91@gmail.com>
2024-12-20 21:36:36 +0530

    cicd

--- research/04_architecture/create_aws_infra.py
+++ research/04_architecture/create_aws_infra.py
@@ -1,21 +1,14 @@
 from aws_s3.create_bucket import create_s3_bucket
-from aws_roles.create_update_role import create_iam_role
-from aws_lambda.create_update_lambda import  create_or_update_lambda_function
+from aws_roles import create_update_role
+from aws_lambda import  create_update_lambda
 
-bucket_name = "my-edquest-bucket-name-1" 
-print(f"bucket_name --- ", bucket_name)
-# Step 1: Create IAM role
-create_s3_bucket(bucket_name)
+if __name__ == "__main__":
+    # Step 1: Create IAM role
+    # create_bucket_new1.create_s3_bucket("gen-ai-poc-73033558046723", "us-east-1")
+    bucket_name = "my-edquest-bucket-name-1"  
+    print(f"bucket name -- ", bucket_name)
+    create_s3_bucket(bucket_name)
+    role_arn = create_update_role.create_iam_role()
 
-role_arn = create_iam_role()
-
-# Step 2: Deploy Lambda function
-create_or_update_lambda_function(role_arn)
-
-# if __name__ == "__main__":
-#     # Step 1: Create IAM role
-#     create_bucket_new1.create_s3_bucket("gen-ai-poc-73033558046723", "us-east-1")
-#     role_arn = create_update_role.create_iam_role()
-
-#     # Step 2: Deploy Lambda function
-#     create_update_lambda.create_or_update_lambda_function(role_arn)
+    # Step 2: Deploy Lambda function
+    create_update_lambda.create_or_update_lambda_function(role_arn)
