36d0566beb694a50d1dc88e14de3354e1eaf8f5b
Raman <rmnchopra91@gmail.com>
2024-12-20 21:33:17 +0530

    cicd

--- research/04_architecture/create_aws_infra.py
+++ research/04_architecture/create_aws_infra.py
@@ -1,11 +1,16 @@
 from aws_s3.create_bucket import create_s3_bucket
-from aws_roles import create_update_role
-from aws_lambda import create_update_lambda
+from aws_roles.create_update_role import create_iam_role
+from aws_lambda.create_update_lambda import  create_or_update_lambda_function
 
-print("Hello world")
 bucket_name = "my-edquest-bucket-name-1" 
 print(f"bucket_name --- ", bucket_name)
+# Step 1: Create IAM role
 create_s3_bucket(bucket_name)
+
+role_arn = create_iam_role()
+
+# Step 2: Deploy Lambda function
+create_or_update_lambda_function(role_arn)
 
 # if __name__ == "__main__":
 #     # Step 1: Create IAM role
