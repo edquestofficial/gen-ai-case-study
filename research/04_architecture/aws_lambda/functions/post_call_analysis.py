77cfdfe471083a7087ce809995babf2a28e543e3
Raman <rmnchopra91@gmail.com>
2024-12-21 18:46:29 +0530

    update lambda

--- research/04_architecture/aws_lambda/functions/post_call_analysis.py
+++ research/04_architecture/aws_lambda/functions/post_call_analysis.py
@@ -3,6 +3,7 @@
 import json
 import urllib.parse
 import boto3
+import uuid
 
 print('Loading function')
 
@@ -18,7 +19,24 @@ def lambda_handler(event, context):
     try:
         response = s3.get_object(Bucket=bucket, Key=key)
         print("CONTENT TYPE: " + response['ContentType'])
-        return response['ContentType']
+        transcribe_client = boto3.client('transcribe', region_name='us-east-1')
+        job_name = 'transcription-job-' + str(uuid.uuid4())
+        audio_file_uri = f's3://my-edquest-bucket-name-1111/dialog.mp3'
+
+        response = transcribe_client.start_transcription_job(
+            TranscriptionJobName=job_name,
+            Media={'MediaFileUri': audio_file_uri},
+            MediaFormat='mp3',
+            LanguageCode='en-US',
+            OutputBucketName="my-edquest-bucket-name-1111",
+            # OutputKey=audio_to_text_dir,
+            Settings={
+                'ShowSpeakerLabels': True,
+                'MaxSpeakerLabels': 2
+            }
+        )
+        print("CONTENT TYPE: " + response)
+        # return response['ContentType']
     except Exception as e:
         print(e)
         print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
