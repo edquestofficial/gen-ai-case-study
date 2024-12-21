6c8871af10053dd06d8631c8970c26919b7c1741
arpits911 <arpits.n.246@gmail.com>
2024-12-21 14:23:32 +0530

    trigger added for s3-lambda

--- research/04_architecture/aws_lambda/create_update_lambda.py
+++ research/04_architecture/aws_lambda/create_update_lambda.py
@@ -9,10 +9,19 @@ ROLE_NAME = "LambdaExecutionRole"
 POLICY_NAME = "LambdaBasicPolicy"
 LAMBDA_CODE_DIR = "/functions"  # Update this path accordingly
 ZIP_FILE_NAME = "post_call_analysis.zip"
+S3_BUCKET_NAME = "my-edquest-bucket-name-111"  # Replace with your S3 bucket name
+S3_EVENT = "s3:ObjectCreated:*"  # Event to trigger Lambda function on object creation
 
 # Initialize AWS clients
 iam_client = boto3.client('iam', region_name=AWS_REGION)
 lambda_client = boto3.client('lambda', region_name=AWS_REGION)
+s3_client = boto3.client('s3', region_name=AWS_REGION)
+
+def get_aws_account_id():
+    """Retrieve the AWS account ID from the environment variables."""
+    sts_client = boto3.client('sts')
+    account_id = sts_client.get_caller_identity()["Account"]
+    return account_id
 
 # Specify the directory where the post_call_analysis.py resides
 
@@ -68,3 +77,41 @@ def create_or_update_lambda_function(role_arn):
         )
 
     print(f"Lambda function {LAMBDA_FUNCTION_NAME} is ready!")
+
+    # Add S3 trigger to Lambda function
+    add_s3_trigger_to_lambda()
+
+def add_s3_trigger_to_lambda():
+    """Add an S3 trigger to the Lambda function."""
+    try:
+        # Get the AWS Account ID
+        aws_account_id = get_aws_account_id()
+        # Grant the Lambda function permission to be triggered by the S3 bucket
+        lambda_client.add_permission(
+            FunctionName=LAMBDA_FUNCTION_NAME,
+            StatementId="S3TriggerPermission",  # Unique ID for this permission
+            Action="lambda:InvokeFunction",
+            Principal="s3.amazonaws.com",
+            SourceArn=f"arn:aws:s3:::{S3_BUCKET_NAME}",
+        )
+        print(f"Permission granted to trigger {LAMBDA_FUNCTION_NAME} from S3 bucket {S3_BUCKET_NAME}")
+
+        # Add S3 bucket notification to trigger Lambda on object creation
+        s3_client.put_bucket_notification_configuration(
+            Bucket=S3_BUCKET_NAME,
+            NotificationConfiguration={
+                'LambdaFunctionConfigurations': [
+                    {
+                        'LambdaFunctionArn': f"arn:aws:lambda:{AWS_REGION}:{aws_account_id}:function:{LAMBDA_FUNCTION_NAME}",
+                        'Events': [S3_EVENT],  # Specify the event you want to trigger the Lambda function
+                    },
+                ],
+            },
+        )
+        print(f"S3 trigger added for Lambda function {LAMBDA_FUNCTION_NAME} on events: {S3_EVENT}")
+
+    except Exception as e:
+        print(f"Error adding S3 trigger: {str(e)}")
+
+# You may also need to update the IAM policy for your Lambda function's role
+# to allow S3 actions (like "s3:GetObject") if it will interact with objects.
