3da378102958d7fbe694e76802ab4ea061c7733d
sunil-chopra1 <chopra2.sunil@gmail.com>
2024-12-26 11:03:59 +0530

    change in lambda function

--- research/04_architecture/aws_lambda/edquest_create_update_lambda.py
+++ research/04_architecture/aws_lambda/edquest_create_update_lambda.py
@@ -57,7 +57,7 @@ def create_or_update_lambda_function(role_arn):
         print(status_code)
 
         if status_code == 200 :
-            print("Lambda function creation response ----- ", response)
+            print("Lambda function creation response -------", response)
              # Update function code
             # lambda_client.update_function_code(
             #     FunctionName=LAMBDA_FUNCTION_NAME,
