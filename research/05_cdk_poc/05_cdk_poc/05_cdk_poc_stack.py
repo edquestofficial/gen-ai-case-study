51bcd32ce03ad269aaa3c8aa858c991ea77174dc
Raman <rmnchopra91@gmail.com>
2025-01-04 18:05:00 +0530

    add scaleup and destroy github actions

--- research/05_cdk_poc/05_cdk_poc/05_cdk_poc_stack.py
+++ research/05_cdk_poc/05_cdk_poc/05_cdk_poc_stack.py
@@ -1,6 +1,8 @@
 from aws_cdk import (
     # Duration,
     Stack,
+    aws_lambda as _lambda, # Import the Lambda module
+    CfnOutput # Import CfnOutput
     # aws_sqs as sqs,
 )
 from constructs import Construct
@@ -17,3 +19,26 @@ class 05CdkPocStack(Stack):
         #     self, "05CdkPocQueue",
         #     visibility_timeout=Duration.seconds(300),
         # )
+        # Define the Lambda function resource
+        my_function = _lambda.Function(
+            self, "CdkPOCLambdaFunction", 
+            runtime = _lambda.Runtime.NODEJS_20_X, # Provide any supported Node.js runtime
+            handler = "index.handler",
+            code = _lambda.Code.from_inline(
+                """
+                exports.handler = async function(event) {
+                    return {
+                        statusCode: 200,
+                        body: JSON.stringify('Hello World!'),
+                    };
+                    };
+                """
+            ),
+        )
+
+        my_function_url = my_function.add_function_url(
+            auth_type = _lambda.FunctionUrlAuthType.NONE,
+        )
+
+        # Define a CloudFormation output for your URL
+        CfnOutput(self, "myFunctionUrlOutput", value=my_function_url.url)
